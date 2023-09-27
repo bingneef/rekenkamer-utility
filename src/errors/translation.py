ceefa718ced002e8e3eb55d49f6eabd7184fef68
Bing Steup <bingsteup@gmail.com>
2023-09-27 14:51:26 +0200

    Fix: linting issue

--- src/errors/translation.py
+++ src/errors/translation.py
@@ -1,7 +1,8 @@
 code_translation = {
     "ENGINE_UNKNOWN_ERROR": "Er is een onbekende fout opgetreden. Probeer het later opnieuw.",
     "ENGINE_FORMAT_ERROR": "De bron mag alleen bestaan uit kleine letters, nummers en koppeltekens (-).",
-    "ENGINE_FORMAT_DASH_ERROR": "De naam van een bron mag niet starten of eindigen met een koppelteken (-). Ook mogen niet meerdere koppeltekens achter elkaar.",
+    "ENGINE_FORMAT_DASH_ERROR": """De naam van een bron mag niet starten of eindigen met een koppelteken (-).
+         Ook mogen niet meerdere koppeltekens achter elkaar.""",
     "ENGINE_ALREADY_EXISTS": "Deze bron bestaat al.",
 }
 
