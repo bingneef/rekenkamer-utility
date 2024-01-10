84acb2ee8d2ecf40d24357cd8691f1b09c62c336
Bing Steup <bingsteup@gmail.com>
2024-01-10 14:46:06 +0100

    Fix: use email from args in generate_verify_code script

--- generate_verify_code.py
+++ generate_verify_code.py
@@ -1,14 +1,20 @@
 from src.util.auth import get_verification_code
 import dotenv
+import sys
 
 
 def main():
     dotenv.load_dotenv()
 
-    print("Wat is de email?")
-    email = input()
+    if len(sys.argv) < 2:
+        print("Please provide an email as a command-line argument.")
+        return
 
+    email = sys.argv[1]
+
+    print("---------------------------")
     print(f"Verification code for {email} is {get_verification_code(email)}")
+    print("---------------------------")
 
 
 if __name__ == "__main__":
